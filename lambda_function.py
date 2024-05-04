a0395e0546bdda927b5ffae598f913c6cf3de4bb
Harikrishna Tangellapally <harikrishna.tangellapally@wisseninfotech.com>
2024-05-04 14:46:07 +0530

    test7

--- lambda_function.py
+++ lambda_function.py
@@ -1,5 +1,8 @@
-def lambda_handler(event,context):
-    d = {'col1':[1,2],'col2':[3,4]}
-    df = pd.DataFrame(data=d)
-    print(df)
-    print('done x1')
+import json
+
+def lambda_handler(event, context):
+    # TODO implement
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
