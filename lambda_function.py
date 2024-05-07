03a1f2c1505c2123a502b49aff7ab5b40e8b409b
Harikrishna Tangellapally <harikrishna.tangellapally@wisseninfotech.com>
2024-05-07 14:15:51 +0530

    test9

--- lambda_function.py
+++ lambda_function.py
@@ -4,5 +4,5 @@ def lambda_handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('testing lambda')
+        'body': json.dumps('testing lambda1')
     }
